3ad7e1ac664a205cf6689c3b3a6ac1303d234fc4
Poojasri0705 <kolliparapoojasri@gmail.com>
2024-09-22 19:19:54 +0530

    Add files via upload

---
+++ day-11.js
@@ -0,0 +1,26 @@
+var favouriteFood = "biryani";
+console.log(favouriteFood)
+
+var myAge = 19;
+console.log(myAge)
+
+
+console.log("1" + "4"+ "1")
+console.log("A" -"B")
+console.log( "A" - "B" + "1")
+console.log( "A" -"B" + 1)
+console.log( "1" - "1")
+console.log(+ "1" + "1" + "1")
+console.log  ( 1 + + "1" + "1" )
+console.log( 1 - "1" + "1")
+console.log(+ "")
+
+
+var student = Boolean(true);
+console.log(student)
+
+var myPet ;
+console.log(myPet)
+
+var carBrand = null ;
+console.log(carBrand)
