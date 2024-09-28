3ac353254a5671005d4d00f23b544d1d2f7c0023
Poojasri0705 <kolliparapoojasri@gmail.com>
2024-09-28 10:05:47 +0530

    Add files via upload

---
+++ day-12.js
@@ -0,0 +1,43 @@
+var num= -76;
+if ( num > 0) {
+    console.log(num + "is positive ");
+}
+else if (num < 0){
+    console.log(num + "is negative");
+}
+else {
+    console.log(num + " is zero")
+}
+
+
+
+
+var age=38;
+if (age<0)
+{
+    console.log(age + "is Child")
+}
+else if (age<18)
+{
+    console.log(age + "is teenagers")
+}
+else if (age<25)
+{
+    console.log(age + "is youngesters")
+}
+else if (age<35)
+{
+    console.log(age + "is adults")
+}
+else if (age<100)
+{
+console.log(age + "is womens")
+}
+
+
+var leapYear=2023;
+console.log( leapYear % 4 === 0 && leapYear % 100 !== 0) ||leapYear % 400 === 0;
+
+
+
+
